fed6d82fe92706d0a869214e8625be086b98f619
Maharsh Patel <patel@maharsh.net>
2020-11-24 21:15:13 -0700

    centered images and corrected order of projects

--- pages/index.js
+++ pages/index.js
@@ -41,14 +41,14 @@ export default function Home() {
 
               <Row className="project">
                 <Col>
-                  <img className="project-image" src="/images/projects/inhale/main-cover.png"></img>
-                  <h3>Inhale</h3>
+                  <img className="project-image" src="/images/projects/castroom/main-cover.png"></img>
+                  <h3>Castroom</h3>
                 </Col>
               </Row>
               <Row className="project">
                 <Col>
-                  <img className="project-image" src="/images/projects/castroom/main-cover.png"></img>
-                  <h3>Castroom</h3>
+                  <img className="project-image" src="/images/projects/inhale/main-cover.png"></img>
+                  <h3>Inhale</h3>
                 </Col>
               </Row>
               <Row className="project">
