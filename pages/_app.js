854d72bb0be616973324d134a8ec79bb499e59cf
Maharsh Patel <patel@maharsh.net>
2020-12-27 21:21:31 -0700

    migrate to mdx for post rendering

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/global.scss"
 
-
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
--- pages/projects/[id].js
+++ pages/projects/[id].js
@@ -1,5 +1,11 @@
-import Layout from '../../components/layout'
+import renderToString from "next-mdx-remote/render-to-string";
+import hydrate from "next-mdx-remote/hydrate";
+import matter from "gray-matter";
 import { getAllProjectIds, getProjectData } from '../../lib/projects'
+import NavBar from "../../components/navbar"
+import Layout from '../../components/layout'
+
+const components = {NavBar};
 
 // gets the list of all valid paths (IDs of each post)
 export async function getStaticPaths() {
@@ -12,21 +18,29 @@ export async function getStaticPaths() {
 
 // gets the post data given the ID
 export async function getStaticProps({ params }) {
-  const projectData = await getProjectData(params.id)
+  const projectData = await getProjectData(params.id);
+  const { data, content } = matter(projectData);
+  const mdxSource = await renderToString(content, {
+    components,
+    scope: data
+  });
+
   return {
     props: {
-      projectData
+      source: mdxSource,
+      frontMatter: data
     }
-  }
+  };
 }
 
-export default function Project({ projectData }) {
+export default function Project({ source, frontMatter }) {
+  const content = hydrate(source, { components });
+
   return (
     <Layout>
-      <title>{projectData.title}</title>
       <article>
-        <h1>{projectData.title}</h1>
-        <div dangerouslySetInnerHTML={{ __html: projectData.contentHtml }} />
+        <h1>{frontMatter.title}</h1> 
+        <div>{content}</div>
       </article>
     </Layout>
   )
