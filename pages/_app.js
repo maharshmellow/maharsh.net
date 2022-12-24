8e5cebc559521f704eba59a400cc6cfacba158ad
Maharsh Patel <maharshmellow@users.noreply.github.com>
2022-12-23 22:15:44 -0700

    feat: add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,24 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/global.scss"
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=G-9LK5Q817ES`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-9LK5Q817ES');
+        `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
